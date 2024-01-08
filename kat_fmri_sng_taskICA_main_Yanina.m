b634b5cb93bc04963e5bfbb0b2c166f184f8b5cc
yanina-prystauka <yanina.prystauka@uconn.edu>
2024-01-08 13:16:57 +0000

    Added a modified file
    The most significant change is the removal of a code adding filepaths

--- kat_fmri_sng_taskICA_main_Yanina.m
+++ kat_fmri_sng_taskICA_main_Yanina.m
@@ -1,50 +1,36 @@
-%% Code  to run group ICA on task-based/resting state fMRI using GIFT
-
+%% Code to run group ICA on task-based/resting state fMRI using GIFT
+%% modified for the FAB project
 % Dependencies:
 % available at https://github.com/kamentsvetanov/external/tree/master/mat
 % spm12
 % gica 
 % matlabcentral
-genpath(addpath('path_to_smp12'));
-genpath(addpath('gica'));
-genpath(addpath('matlabcentral')); % Contains rdir
+%genpath(addpath('spm'));
+addpath(genpath('C:\Users\segaertk-admin\Documents\NI_software\spm12'))
+addpath(genpath('C:\Users\segaertk-admin\Documents\NI_software\gica'));
+addpath(genpath('C:\Users\segaertk-admin\Documents\NI_software\matlabcentral')); % Contains rdir
 
 %% add EM fMRI filepaths to T table
 clear
 % My code is developed around the following two variables (S and T)
 % S - a structure containing study/project variables including paths, acquisition details and paramters for different processing steps (saved in substructures e.g. S.ica contains all about ICA)
 % T - a table containing subject specific information e.g. Subject ID, age,cognitive mesures, to subject-specific filepaths.
-T = load('path_to/ica_setup_20231205.mat'); % Build your own structure S and table T similar to those in ica_setup_20231205.mat 
+%create a mat file from an excel file with participant info
+excelFilePath = 'C:/Users/segaertk-admin/Documents/ToT/taskICA_part1_Yanina/participants'
+dataTable = readtable(excelFilePath);
+matFilePath = 'C:/Users/segaertk-admin/Documents/ToT/taskICA_part1_Yanina/ica_setup_20240102.mat';
+save(matFilePath, 'dataTable');
 
-% --------------------------------------------------------------------
-% Add filepaths of processed EPI data for each session in the T table
-% --------------------------------------------------------------------
-%get all filepaths for TOT1 (series 3), in my examples is EmoMem abbreviated to 'em' 
-f_em1 = rdir('path_to_root_directory_of_nifti_processed images/*.nii');
-f_em1 = {f_em1.name}';
-% Get associated Subject IDss. in thi example they are prefixed by CBU
-subid = regexp(f_em1,'CBU\d\d\d\d\d\d','match');
-subid = [subid{:}]';
-% Create new table for TOT1 filepaths and CCids
-Tem1  = table(subid,f_em1,'VariableNames',{'SessionC','f_em1'});
-T=innerjoin(T,Tem1,'Keys','SessionC');
+T = load('C:/Users/segaertk-admin/Documents/ToT/taskICA_part1_Yanina/ica_setup_20240102.mat'); % Build your own structure S and table T similar to those in ica_setup_20231205.mat 
 
-%get all filepaths for TOT2 (series 4) (in this exmaple is EmoMem
-f_em2 = rdir('/imaging/camcan/archive/cc280/mri/pipeline/release003/Session_C_EmoMem/aamod_smooth_00001/*/EmoMemSeries4/*.nii');
-f_em2 = {f_em2.name}';
-% Get associated CCids
-subid = regexp(f_em2,'CBU\d\d\d\d\d\d','match');
-subid = [subid{:}]';
-% Create new table for EM filepaths and CCids
-Tem2  = table(subid,f_em2,'VariableNames',{'SessionC','f_em2'});
-T=innerjoin(T,Tem2,'Keys','SessionC');
-
+% removed the part adding filepaths of processed EPI data
 
 % Exclude subjects for which the ICA fails
 % excl_subj = {'CC510480',... 
 %             };
 % id        = strmatch(excl_subj,T.SubCCIDc);
 % T(id,:)   = [];
+
 % ICA Setup
  % ICA Setup
     ICA                   = [];
@@ -52,21 +38,21 @@ T=innerjoin(T,Tem2,'Keys','SessionC');
     ICA.ncomps            = [5];%,30,40,50,75,100];Number of ICA components requested (if do_mdl=1 this is overwritten) 
     ICA.prefix            = {'gica_tot'};%;{'CC700', 'CC280', 'CC700_CC280'}; % Give a prefix to the name of the analysis
     ICA.giftprefix        = 'tot'; % Type of ICA depending on the data input. For 3D data, e.g. structural data use 'sbm'
-    ICA.scan_vec          = 1:320;% Total number of volumes  
+    ICA.scan_vec          = 1:500;% Total number of volumes  
     ICA.nscans            = numel(ICA.scan_vec);
-    ICA.filepaths         =  [strcat('', T.f_em1 , ''',''' , T.f_em2 )]; % A cell array of filepaths to brain images. For multiples runs/sessions use multiple columns
+    ICA.filepaths         =  [strcat('', dataTable.f_tot_ses1_run1 , ''',''' , dataTable.f_tot_ses1_run2, ''',''' , dataTable.f_tot_ses1_run3 , ''',''' , dataTable.f_tot_ses1_run4 , ''',''' , dataTable.f_tot_ses2_run1, ''',''' , dataTable.f_tot_ses2_run2, ''',''' , dataTable.f_tot_ses2_run3,  ''',''' , dataTable.f_tot_ses2_run4)]; % A cell array of filepaths to brain images. For multiples runs/sessions use multiple columns
     ICA.nsubjects         = size(T,1); % Number of subjects 
-    ICA.SubID             = T.SubCCIDc; % Subject ID, good to cross-reference at later point
+    ICA.SubID             = dataTable.SubID; % Subject ID, good to cross-reference at later point
     ICA.do_parallel       = 1; % 1-do parallel | 0 - do serial
     ICA.num_workers       = 128;
     ICA.do_icasso         = 0; % Change at a later stage to 1 to ensure the components are stable
     ICA.num_icasso_runs   = 128; % Number of ICASSO repetitions
-    ICA.root_dir          = 'path_to_output_directory'; % Name of output folder
-    ICA.groupsInfo.name   = 'Age'; % (optional) Could use additional variable to correlate Loading with
-    ICA.groupsInfo.val    = T.Age'; % (optional) Varible to be correlated with subject loading values
+    ICA.root_dir          = 'C:\\Users\\segaertk-admin\\Documents\\ToT\\taskICA_part1_Yanina\\output'; % Name of output folder
+    %ICA.groupsInfo.name   = 'Age'; % (optional) Could use additional variable to correlate Loading with
+    %ICA.groupsInfo.val    = T.Age'; % (optional) Varible to be correlated with subject loading values
 
 % Path and name of gift batch template file
-S.paths.scripts = 'path_to_this_function,which_also_contains_associated_GIFT_directory';
+S.paths.scripts = 'C:\Users\segaertk-admin\Documents\ToT\taskICA_part1_Yanina';
     ICA.gica_template_fname = fullfile(S.paths.scripts, 'GIFT','kat_fmri_rest_gift_batch_template.m'); 
 
     % Filepaths to Input data for all subjects
@@ -76,7 +62,7 @@ S.paths.scripts = 'path_to_this_function,which_also_contains_associated_GIFT_dir
 %     ICA.f_mask = [strcat('''', f_mask ,'''')] ;
     global GICA_PARAM_FILE;
 
-for ii = 1:length(S.ica.ncomps)
+for ii = 1:length(ICA.ncomps)
     ICA.ncomp     = ICA.ncomps(ii);  
     ICA.ncomp_pca = round(ICA.ncomp*1.5); % Number of Principle Components = number of ICs with a factor of 1.5
     for jj = 1:numel(ICA.prefix)
